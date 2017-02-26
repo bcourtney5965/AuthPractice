975314557e799b5918568a25cfbd1d675e5710a6
Brandon Courtney <brandon.h.courtney@gmail.com>
2017-02-25 23:37:26 -0800

    Mock users and stategies added

--- index.js
+++ index.js
@@ -10,6 +10,36 @@ var JwtStrategy = passportJWT.Strategy;
 
 var port = 3000;
 
+var users = [
+  {
+    id: 1,
+    name: 'jonathanmh',
+    password: '%2yx4'
+  },
+  {
+    id: 2,
+    name: 'test',
+    password: 'test'
+  }
+];
+
+var jwtOptions = {}
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeader();
+jwtOptions.secretOrKey = 'tasmanianDevil';
+
+var strategy = new JwtStrategy(jwtOptions, function(jwt_payload, next) {
+  console.log('payload received', jwt_payload);
+  // usually this would be a database call:
+  var user = users[_.findIndex(users, {id: jwt_payload.id})];
+  if (user) {
+    next(null, user);
+  } else {
+    next(null, false);
+  }
+});
+
+passport.use(strategy);
+
 app.get("/", function(req, res) {
   res.json({message: "Express is up!"});
 });
